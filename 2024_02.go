1fc609e9cf9bcd65296d3073846cef9bb0bd42d0
misfasol <gabriel.schlagenhaufer@gmail.com>
2024-12-02 20:40:11 -0300

    2 feito em go e terminado em haskell

---
+++ 2024_02.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Ex2() {
+	dados, err := os.ReadFile("2024_02.txt")
+	if err != nil {
+		panic(err)
+	}
+	conteudo := string(dados)
+	linhas := strings.FieldsFunc(conteudo, func(r rune) bool {
+		return r == '\n'
+	})
+	var numeros [][]int
+	for _, i := range linhas {
+		nstr := strings.Fields(i)
+		var nint []int
+		for _, v := range nstr {
+			conv, err := strconv.Atoi(v)
+			if err != nil {
+				panic(err)
+			}
+			nint = append(nint, conv)
+		}
+		numeros = append(numeros, nint)
+	}
+	// for k, v := range numeros {
+	// 	fmt.Println(k, ":", v)
+	// }
+	checar := func(lista []int) bool {
+		nova := make([]int, len(lista))
+		for k := range lista {
+			nova[k] = lista[k]
+		}
+		slices.Reverse(nova)
+		if !slices.IsSorted(lista) && !slices.IsSorted(nova) {
+			return false
+		}
+		for k := range len(lista) - 1 {
+			if math.Abs(float64(lista[k+1]-lista[k])) < 1 || math.Abs(float64(lista[k+1]-lista[k])) > 3 {
+				return false
+			}
+		}
+		return true
+	}
+	contador := 0
+	dois := 0
+	for _, v := range numeros {
+		// criar variacoes
+		for i := 0; i < len(v); i++ {
+			vari := slices.Concat(v[:i], v[i+1:])
+			if checar(vari) {
+				dois++
+				break
+			}
+		}
+		if checar(v) {
+			contador++
+		}
+	}
+	fmt.Printf("cont: %v\n", contador)
+	fmt.Printf("dois: %v\n", dois)
+}
--- 2024_02.hs
+++ 2024_02.hs
@@ -3,11 +3,10 @@ import Data.Ord
 import System.IO
 
 -- funcao1 :: String -> Int
-funcao1 x =
-  let linhas = map (map (\b -> read b :: Int) . words) (lines x)
-      sorted = filter (\a -> a == sort a || a == sortBy (comparing Data.Ord.Down) a) linhas
-      entre = filter (\a -> maiMenDif a max <= 3 && maiMenDif a min >= 1) sorted
-   in length entre
+funcao1 x = length $ filter checar x
+
+checar :: [Int] -> Bool
+checar l = (l == sort l || l == sortBy (comparing Data.Ord.Down) l) && maiMenDif l max <= 3 && maiMenDif l min >= 1
 
 maiMenDif :: [Int] -> (Int -> Int -> Int) -> Int
 maiMenDif [] _ = 0
@@ -15,15 +14,23 @@ maiMenDif [x] _ = 0
 maiMenDif [x, y] _ = abs (x - y)
 maiMenDif (x : y : xs) op = op (abs (x - y)) (maiMenDif (y : xs) op)
 
-funcao2 :: String -> Int
-funcao2 x =
-  let linhas = map (map (\b -> read b :: Int) . words) (lines x)
-      sorted = filter (\a -> a == sort a || a == sortBy (comparing Data.Ord.Down) a) linhas
-      entre = filter (\a -> maiMenDif a max <= 3 && maiMenDif a min >= 1) sorted
-   in length entre
+-- funcao2 :: String -> Int
+funcao2 mat =
+  let filtado = filter removeIndCheca mat
+   in length filtado
+
+removeIndCheca :: [Int] -> Bool
+removeIndCheca lin = any (checar . removeInd lin) [0 .. length lin - 1]
+
+slice :: Int -> Int -> [a] -> [a]
+slice from to xs = take (to - from) (drop from xs)
+
+removeInd :: [Int] -> Int -> [Int]
+removeInd l i = slice 0 i l ++ slice (i + 1) (length l) l
 
 main = do
   arq <- openFile "2024_02.txt" ReadMode
   conteudo <- hGetContents arq
-  print $ funcao1 conteudo
-  print $ funcao2 conteudo
+  let linhas = map (map (\b -> read b :: Int) . words) (lines conteudo)
+  print $ funcao1 linhas
+  print $ funcao2 linhas
